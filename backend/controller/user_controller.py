c360b81d4fee5a0cb31c32bc39a93d6f1a751067
Sairaj <178469889+ThakarSairaj@users.noreply.github.com>
2026-01-01 18:44:21 +0530

    add password hashing

--- backend/controller/user_controller.py
+++ backend/controller/user_controller.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import Session
 from database_con.database import session, engine
 from models.models import User, UserHealthInfo
 from schemas.users import CreateUser, CreateUserHealthInfo
+from core.security import hash_password
 
 router=APIRouter()
 
@@ -21,6 +22,7 @@ db_dependency=Annotated[Session, Depends(get_db)]
 
 @router.post("/createUser/", status_code=status.HTTP_201_CREATED)
 def create_user(user: CreateUser, db: db_dependency):
+    hashed_password=hash_password(user.password)
     db_user=User(
         first_name=user.first_name,
         last_name=user.last_name,
@@ -28,13 +30,17 @@ def create_user(user: CreateUser, db: db_dependency):
         gender=user.gender,
         phone=user.phone,
         email=user.email,
-        password=user.password,
+        password=hashed_password,
         country=user.country,
         state=user.state,
         city=user.city
+        
+       
     )
-
+        
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
+    
+
 
