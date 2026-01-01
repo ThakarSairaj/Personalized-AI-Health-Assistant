fb7aa586bca93955bf6a606a7e30b2090e04e097
Sairaj <178469889+ThakarSairaj@users.noreply.github.com>
2026-01-01 17:45:50 +0530

    add database connection

---
+++ backend/database_con/database.py
@@ -0,0 +1,17 @@
+# This file is used to connect to the database only
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+url='mysql+pymysql://root:root@localhost:3306/users' # This is the url for the local database connection
+
+engine=create_engine(url, echo=True)
+
+session=sessionmaker(autoflush=False, 
+                     autocommit=False, 
+                     bind=engine) # This is use to execute the query from the python
+
+base=declarative_base()
+
