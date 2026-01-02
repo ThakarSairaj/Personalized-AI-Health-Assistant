e874224e84fce402b3ccc13f573699751f9b5f01
Sairaj <178469889+ThakarSairaj@users.noreply.github.com>
2026-01-02 13:49:53 +0530

    integrated health detail controller

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
+# main.py
 from fastapi import FastAPI
 from database_con.database import base, engine
 from controller.user_controller import router as user_router
+from controller.health_basic_details_controller import router as health_router
 
 app=FastAPI()
 
 base.metadata.create_all(bind=engine)
 
 app.include_router(user_router)
+
+app.include_router(health_router)
