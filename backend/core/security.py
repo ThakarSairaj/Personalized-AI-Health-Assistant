3ff58e4837a9d14e3d7258fdb6ca930f99f5eba5
Sairaj <178469889+ThakarSairaj@users.noreply.github.com>
2026-01-02 20:35:33 +0530

    add password verify module for authentication purpose

--- backend/core/security.py
+++ backend/core/security.py
@@ -7,3 +7,6 @@ pwd_cryp=CryptContext(
 
 def hash_password(plain_password: str) -> str:
     return pwd_cryp.hash(plain_password)
+
+def verify_password(plain_password: str, database_password: str)-> bool:
+    return pwd_cryp.verify(plain_password, database_password)
