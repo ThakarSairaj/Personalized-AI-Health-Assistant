064547decd33484bb5a2c5bca5dffc361230bece
susmit nikambe <susmitnikambe.123@gmail.com>
2026-02-05 13:10:05 +0530

    add CROS origin

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,17 @@ from database_con.database import base, engine
 from controller.user_controller import router as user_router
 from controller.health_basic_details_controller import router as health_router
 from controller.auth import router as auth_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ⚠️ only for development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 base.metadata.create_all(bind=engine)
 
