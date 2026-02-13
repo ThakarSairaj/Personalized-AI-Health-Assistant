57c71d3f2f262961e5e892e1baa80a85b9a57b6b
Sairaj <178469889+ThakarSairaj@users.noreply.github.com>
2026-02-13 16:06:59 +0530

    responsible to manage session with DB from login to logout

---
+++ backend/core/dependencies.py
@@ -0,0 +1,54 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from typing import Annotated
+
+from database_con.database import session
+from models.models import User
+from core.jwt import to_decode
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login/")
+
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+
+    try:
+        payload = to_decode(token)
+        user_id = payload.get("user_id")
+
+        if user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid token"
+            )
+
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token"
+        )
+
+    user = db.query(User).filter(User.user_id == user_id).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found"
+        )
+
+    return user
