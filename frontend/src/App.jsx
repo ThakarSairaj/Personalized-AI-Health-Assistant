95fd1c15bb99eb5c76128d0d91de689505f1ff47
Sairaj <178469889+ThakarSairaj@users.noreply.github.com>
2026-02-14 19:58:44 +0530

    give default route to the landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import AuthForm from './components/AuthForm'
 import Register from './components/Register'
 import HealthInfo from './components/HealthInfo'
 import UploadReport  from './components/UploadReport'
-
+import Landing from './components/Landing'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AuthForm />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<AuthForm />} />
         <Route path="/register" element={<Register />} />
         <Route path="/health-info" element={<HealthInfo />} />
         <Route path="/upload-report" element={<UploadReport />} />
